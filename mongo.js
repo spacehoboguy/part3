b6c27154df354e03c55e13a507543efca4cb1d51
spacehoboguy <ph.olafsen@gmail.com>
2023-09-17 15:34:42 +0200

    added mongoDB

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://pholafsen:${password}@fsopennotes.rzdzudh.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number,
+})
+
+if (!name && !number) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to the phonebook!`)
+        mongoose.connection.close()
+    })
+}
+
